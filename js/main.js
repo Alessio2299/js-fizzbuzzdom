aa5670dfa8b393732decc633314a96c799c6ad51
Alessio Napoli <alessionapoli99@icloud.com>
2022-01-21 16:47:46 +0100

    Ended Exercise with two possible solutions

--- js/main.js
+++ js/main.js
@@ -1,29 +1,38 @@
 // Assegno una variabile per document
 let containerElements = document.getElementById("container");
+
 // Creo un prompt per indicare quanti quadrati voglio
+
 let number = parseInt(prompt("Quanti quadrati vuoi inserire?"));
+
 // Creo un Ciclo
+
 for ( i=1; i <= number; i++){
 
   // Creo una Condizione
+
+  //  Soluzione con If
   /*
-  if (i % 3 == 0 && i % 5 == 0){
-  containerElements.innerHTML += "<div class='box red'>fizzbuzz</div>";
-  } else if (i % 3 == 0){
+  if (i % 3 == 0 && i % 5 != 0){
   containerElements.innerHTML += "<div class='box green'>fizz</div>";
-  } else if (i % 5 == 0){
-  containerElements.innerHTML += "<div class='box yellow'>buzz</div>"
-  }  else {
+  } else if (i % 5 == 0 && i % 3 != 0){
+  containerElements.innerHTML += "<div class='box yellow'>buzz</div>";
+  } else if (i % 3 == 0 && i % 5 == 0){
+  containerElements.innerHTML += "<div class='box red'>fizzbuzz</div>";
+  } else {
     containerElements.innerHTML += "<div class='box'>" + i + "</div>";
   }
   */
 
+  // Soluzione con Switch
 
   switch (true) {
-    case (i % 3 == 0):
+    case (i % 3 == 0 && i % 5 != 0):
       containerElements.innerHTML += "<div class='box green'>fizz</div>";
-    case (i % 5 == 0):
+      break;
+    case (i % 5 == 0 && i % 3 != 0):
       containerElements.innerHTML += "<div class='box yellow'>buzz</div>";
+      break;
     case (i % 3 == 0 && i % 5 == 0):
       containerElements.innerHTML += "<div class='box red'>fizzbuzz</div>";
       break;
